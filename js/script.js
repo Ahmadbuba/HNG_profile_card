96439683069a2aae008e205c5b07913ecc5893c2
Ahmadbuba <ahmadbubacherryfield@gmail.com>
2023-09-12 13:38:06 +0100

    feat- entire UI achieved, remaining to work on the details of the submission such datatestId and other instructions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const dayofWeekString = document.getElementById("dayOfWeek");
 const utcmillisecondstime = document.getElementById("secondsUTC");
-utcmillisecondstime.innerHTML = new Date().getTime();
+const now = new Date();
+// utcmillisecondstime.innerHTML = new Date().getTime();
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 days.forEach((day,index)=>{
     // Check if the index of day value is equal to the returned value of getDay()
-    if(index == new Date().getDay()){
+    if(index == now.getDay()){
         console.log("Today is "+day);
-        dayofWeekString.innerHTML = day;
+        dayofWeekString.innerText = day
 
     }
         
     }
     
 )
+utcmillisecondstime.innerHTML = now.getTime();
